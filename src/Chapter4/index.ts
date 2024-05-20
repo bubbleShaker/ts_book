701faae429f5c3226a7ca6b3c4a764fd04b5131e
bubble_shaker <intselectk@gmail.com>
2024-05-20 10:51:37 +0900

    Add Chapter4

--- src/Chapter4/index.ts
+++ src/Chapter4/index.ts
@@ -1,11 +1,366 @@
-type User = { name: string; age: number };
-const users: User[] = [
-  { name: "uhyo", age: 26 },
-  { name: "John Smith", age: 15 }
-];
+const repeatLength = 5;
+const repeat = function<T>(element: T): T[] {
+  const repeatLength = 3;
 
-const names = users.map((u: User): string => u.name);
-console.log(names); // ["uhyo", "John Smith"] と表示される
+  const result: T[] = [];
+  for (let i = 0; i < repeatLength; i++) {
+    result.push(element);
+  }
+  return result;
+};
+
+console.log(repeat("a"));  // ["a", "a", "a"] と表示される
+console.log(repeatLength); // 5 と表示される
+
+
+
+// const repeatLength = 5;
+// const repeat = function<T>(element: T): T[] {
+//   const result: T[] = [];
+//   for (let i = 0; i < repeatLength; i++) {
+//     result.push(element);
+//   }
+//   return result;
+// };
+
+// console.log(repeat("a")); // ["a", "a", "a", "a", "a"] と表示される
+
+
+
+// const repeat = function<T>(element: T, length: number): T[] {
+//   const result: T[] = [];
+//   for (let i = 0; i < length; i++) {
+//     result.push(element);
+//   }
+//   // エラー: Cannot redeclare block-scoped variable 'result'.
+//   const result = [];
+//   return result;
+// };
+
+
+
+// const repeat = function<T>(element: T, length: number): T[] {
+//   // この変数resultはrepeatの変数スコープに属する
+//   const result: T[] = [];
+//   for (let i = 0; i < length; i++) {
+//     result.push(element);
+//   }
+//   return result;
+// };
+
+// // 関数の外には変数resultは存在しない
+// // エラー: Cannot find name 'result'.
+// console.log(result);
+
+
+
+// function double<T>(func: (arg: T) => T): (arg: T) => T {
+//   return (arg) => func(func(arg));
+// }
+
+// type NumberToNumber = (arg: number) => number;
+
+// const plus2: NumberToNumber = double(x => x + 1);
+// console.log(plus2(10)); // 12 と表示される
+
+
+
+// function makeTriple<T>(x: T, y: T, z: T): T[] {
+//   return [x, y, z];
+// }
+
+// const stringTriple = makeTriple("foo", "bar", "baz");
+// const mixed = makeTriple("foo", 123, false);
+
+
+
+// type Func = <T>(arg: T, num: number) => T[];
+
+// const repeat: Func = (element, length) => {
+//   const result = [];
+//   for (let i = 0; i < length; i++) {
+//     result.push(element);
+//   }
+//   return result;
+// };
+
+
+
+// function repeat<T>(element: T, length: number): T[] {
+//   const result: T[] = [];
+//   for (let i = 0; i < length; i++) {
+//     result.push(element);
+//   }
+//   return result;
+// }
+
+// // resultはstring[]型となる
+// const result = repeat("a", 5);
+
+
+
+// const repeat = <T extends {
+//   name: string;
+// }>(element: T, length: number): T[] => {
+//   const result: T[] = [];
+//   for (let i = 0; i < length; i++) {
+//     result.push(element);
+//   }
+//   return result;
+// }
+
+// type HasNameAndAge = {
+//   name: string;
+//   age: number;
+// }
+
+// // これはOK
+// console.log(repeat<HasNameAndAge>({name: "uhyo",age: 26,}, 3));
+// // これはコンパイルエラー
+// // // エラー: Type 'string' does not satisfy the constraint '{ name: string; }'.
+// // console.log(repeat<string>("a", 5));
+
+
+
+// const pair = <Left, Right>(left: Left, right: Right): [Left, Right] => [left, right];
+// // pは[string, number]型
+// const p = pair<string, number>("uhyo", 26);
+
+
+
+// const utils = {
+//   repeat<T>(element: T, length: number): T[] {
+//     const result: T[] = [];
+//     for (let i = 0; i < length; i++) {
+//       result.push(element);
+//     }
+//     return result;
+//   }
+// }
+
+
+
+// const repeat = <T>(element: T, length: number): T[] => {
+//   const result: T[] = [];
+//   for (let i = 0; i < length; i++) {
+//     result.push(element);
+//   }
+//   return result;
+// }
+
+
+
+// const repeat = function<T>(element: T, length: number): T[] {
+//   const result: T[] = [];
+//   for (let i = 0; i < length; i++) {
+//     result.push(element);
+//   }
+//   return result;
+// }
+
+
+
+// function repeat<T>(element: T, length: number): T[] {
+//   const result: T[] = [];
+//   for (let i = 0; i < length; i++) {
+//     result.push(element);
+//   }
+//   return result;
+// }
+
+// // ["a", "a", "a", "a", "a"] が表示される
+// console.log(repeat<string>("a", 5));
+// // [123, 123, 123] が表示される
+// console.log(repeat<number>(123, 3));
+
+
+
+// type User = { name: string };
+// type ReadonlyUser = { readonly name: string };
+
+// const uhyoify = (user: User) => {
+//   user.name = "uhyo";
+// };
+
+// const john: ReadonlyUser = {
+//   name: "John Smith"
+// };
+// // // これはコンパイルエラー（john.nameはreadonlyなので）
+// // // エラー: Cannot assign to 'name' because it is a read-only property.
+// // john.name = "Nanashi";
+
+// // これはエラーにならない！
+// uhyoify(john);
+
+// // readonlyなのにnameが変えられてしまった
+// console.log(john.name); // "uhyo" と表示される
+
+
+
+// function fillZero(nums: number[]): void {
+//   for (let i = 0; i < nums.length; i++) {
+//     nums[i] = 0;
+//   }
+// }
+
+// // fillZeroにはnumber[]型を与えることができる
+// const nums1: number[] = [1, 10, 100];
+// fillZero(nums1);
+// console.log(nums1); // [0, 0, 0] と表示される
+
+// // fillZeroにreadonly number[]型を与えるのはコンパイルエラー
+// const nums2: readonly number[] = [1, 1, 2, 3, 5, 8];
+// // エラー: Argument of type 'readonly number[]' is not assignable to parameter of type 'number[]'.
+// // The type 'readonly number[]' is 'readonly' and cannot be assigned to the mutable type 'number[]'.
+// fillZero(nums2);
+
+
+
+// function sum(nums: readonly number[]): number {
+//   let result = 0;
+//   for (const num of nums) {
+//     result += num;
+//   }
+//   return result;
+// }
+// // sumにはreadonly number[]型を与えることができる
+// const nums1: readonly number[] = [1, 10, 100];
+// console.log(sum(nums1)); // 111 と表示される
+// // sumにはnumber[]型も与えることができる
+// const nums2: number[] = [1, 1, 2, 3, 5, 8];
+// console.log(sum(nums2)); // 20 と表示される
+
+
+
+// type UnaryFunc = (arg: number) => number;
+// type BinaryFunc = (left: number, right: number) => number;
+
+// const double: UnaryFunc = arg => arg * 2;
+// const add: BinaryFunc = (left, right) => left + right;
+
+// // UnaryFuncをBinaryFuncとして扱うことができる
+// const bin: BinaryFunc = double;
+// // 20 が表示される
+// console.log(bin(10, 100));
+
+
+
+// type HasName = {
+//   name: string;
+// }
+// type HasNameAndAge = {
+//   name: string;
+//   age: number;
+// }
+
+// const showName = (obj: HasName) => {
+//   console.log(obj.name);
+// };
+// const g: (obj: HasNameAndAge) => void = showName;
+
+// g({
+//   name: "uhyo",
+//   age: 26,
+// });
+
+
+
+// type SwapFunc = {
+//   (arg: string): number;
+//   (arg: number): boolean;
+// }
+
+
+
+// type F = (arg: string) => number;
+// type G = { (arg: string): number; };
+
+
+
+// type MyFunc = {
+//   isUsed?: boolean;
+//   (arg: number): void;
+// };
+
+// const double: MyFunc = (arg: number) => {
+//   console.log(arg * 2)
+// };
+
+// // doubleはisUsedプロパティを持つ
+// double.isUsed = true;
+// console.log(double.isUsed);
+// // doubleは関数として呼び出せる
+// double(1000);
+
+
+
+// // Parameter 'num' implicitly has an 'any' type.
+// const f = (num) => num * 2;
+
+
+
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const arr2 = nums.filter((x) => x % 3 === 0);
+// console.log(arr2); // [3, 6, 9] と表示される
+
+
+
+// type F = (arg: number) => string;
+// // この関数式は引数の型を書かなくてもOK
+// const xRepeat: F = (num) => "x".repeat(num);
+
+
+
+// function range(min: number, max: number): number[] {
+//   const result = [];
+//   for (let i = min; i <= max; i++) {
+//     result.push(i);
+//   }
+//   return result;
+// }
+
+// const arr = range(5, 10);
+// for (const value of arr) console.log(value);
+
+
+
+// const g = (num: number) => {
+//   for (let i = 0; i < num; i++) {
+//     console.log("Hello, world!");
+//   }
+// };
+// g(5);
+
+
+
+// const xRepeat = (num: number) => "x".repeat(num);
+
+
+
+// type F2 = (arg: string, arg2: string) => boolean;
+// // エラー: Type '(num: number) => void' is not assignable to type 'F2'.
+// const fun: F2 = (num: number): void => console.log(num);
+
+
+
+// type F = (repeatNum: number) => string;
+
+// const xRepeat: F = (num: number): string => "x".repeat(num);
+
+
+
+// const xRepeat = (num: number): string => "x".repeat(num);
+
+
+
+// type User = { name: string; age: number };
+// const users: User[] = [
+//   { name: "uhyo", age: 26 },
+//   { name: "John Smith", age: 15 }
+// ];
+
+// const names = users.map((u: User): string => u.name);
+// console.log(names); // ["uhyo", "John Smith"] と表示される
 
 
 
---
+++ src/Chapter4/test.ts
@@ -0,0 +1,60 @@
+function map<T,U>(array: T[], callback: (item:T)=>U): U[] {
+  let ret:Array<U> = [];
+  for(const elem of array){
+    ret.push(callback(elem));
+  }
+  return ret;
+}
+const data = [1, -3, -2, 8, 0, -1];
+
+const result: boolean[] = map(data, (x) => x >= 0);
+// [true, false, false, true, true, false] と表示される
+console.log(result);
+
+
+
+// function map(array: number[], callback: (item:number)=>number): number[] {
+//   let ret:Array<number> = [];
+//   for(const elem of array){
+//     ret.push(callback(elem));
+//   }
+//   return ret;
+// }
+
+// const data = [1, 1, 2, 3, 5, 8, 13];
+
+// const result = map(data, (x) => x * 10);
+// // [10, 10, 20, 30, 50, 80, 130] と表示される
+// console.log(result);
+
+
+
+// function getFizzBuzzString(i:number):string{
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     return("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     return("Fizz");
+//   } else if (i % 5 === 0) {
+//     return("Buzz");
+//   } else {
+//     return String(i)
+//   }
+// }
+// function sequence(start:number,end:number):number[]{
+//   const ret:Array<number>=[];
+//   for(let i=start;i<=end;i++){
+//     ret.push(i);
+//   }
+//   return ret;
+// }
+// for (const i of sequence(1,100)) {
+//   const message = getFizzBuzzString(i);
+//   console.log(message);
+// }
+
+
+
+// for (let i = 1; i <= 100; i++) {
+//   const message = getFizzBuzzString(i);
+//   console.log(message);
+// }
